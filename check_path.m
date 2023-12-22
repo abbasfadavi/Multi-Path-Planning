3d63ebde94a4ecf5db9c9658b5d3a32d48785e4b
abbas fadavi <89520501+abbasfadavi@users.noreply.github.com>
2023-12-22 15:31:18 +0330

    Update check_path.m

--- check_path.m
+++ check_path.m
@@ -5,12 +5,15 @@ function [e1,e2]=check_path(forbid,pa)
 e1=0;
 for i=1:size(forbid,2)
 f=forbid(i,:);
-k1=px>=f(1) & px<=f(1)+f(3);    
-k2=py>=f(2) & py<=f(2)+f(4);     
+k1=(px>=f(1)) & (px<=f(1)+f(3));    
+k2=(py>=f(2)) & (py<=f(2)+f(4));     
 k3=pz<=f(5);
 k=k1&k2&k3;
 e1=e1+sum(k);
 end
 e2=length(px);
+
+
+
 
 
